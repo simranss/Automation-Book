b7b8fe69789ad315cfbd90d3a19f22ce44b8d885
Simran Sharma <simran.s.sharma00@gmail.com>
2021-12-08 16:36:55 +0530

    Completed

---
+++ Collatz.py
@@ -0,0 +1,27 @@
+#collatz function
+def collatz(a):
+
+    # even
+    if a % 2 == 0:
+        b = a // 2
+        print(b)
+        return b
+    
+    # odd
+    elif a % 2 == 1:
+        b = (3 * a) + 1
+        print(b)
+        return b
+
+    else:
+        return a
+
+
+#Input the number
+number = int(input("Enter a number: "))
+
+c = number
+while True:
+    c = collatz(c)
+    if c == 1:
+        break
